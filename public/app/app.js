14b62da8e305c615e9a699c1cb89387d3cdeb3c3
Pan21 <49293503+Pan21@users.noreply.github.com>
2021-12-12 20:59:00 -0500

    Add files via upload

--- public/app/app.js
+++ public/app/app.js
@@ -42,6 +42,7 @@ function signIn(){
     .collection("employees")
     .get()
     .then((querySnapshot) => {
+      $('#name-contain').empty();
       querySnapshot.forEach((doc) => {
         console.log("working");
 
@@ -105,10 +106,70 @@ function updateSchedule () {
 
 
 
+function addEmployee(){
+
+  let nameV = $("#nameInp").val();
+    let mondayV = $("#mondayInp").val();
+    let tuesdayV = $("#tuesdayInp").val();
+    let wendsdayV = $("#wendsdayInp").val();
+    let thursdayV = $("#thursdayInp").val();
+    let fridayV = $("#fridayInp").val();
+    let aObj = { name: nameV, monday: mondayV, tuesday: tuesdayV, wendsday: wendsdayV, thursday: thursdayV, friday: fridayV};
+
+    _db
+      .collection("employees")
+      .add(aObj)
+      .then(function (doc) {
+        console.log("added doc " + doc.id);
+      });
+
+}
+
+function deleteEmployee(){
+
+  var empDeleteID = $("#employee-delete-select").find('option:selected').attr('id');
+
+  _db.collection("employees").doc(empDeleteID).delete().then(() => {
+    console.log("Document successfully deleted!");
+}).catch((error) => {
+    console.error("Error removing document: ", error);
+});
+
+}
+
+
+
 $(document).ready(function() {
     try {
-        initListiners();
-        let app = firebase.app();
+      _db = firebase.firestore();
+
+      _db
+    .collection("employees")
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        console.log("working");
+
+          $('#name-contain').append(`
+
+          <div class="emp-schedule">
+
+          <p>${doc.data().name}</p>
+            <p>${doc.data().monday}</p>
+            <p>${doc.data().tuesday}</p>
+            <p>${doc.data().wendsday}</p>
+            <p>${doc.data().thursday}</p>
+            <p>${doc.data().friday}</p>
+          
+          </div>
+
+        `)       
+
+          console.log(doc.id, " => ", doc.data());
+      });
+  });
+  var _db = "";
+
     } catch {
         console.log("Error on Try")
     }
@@ -133,6 +194,8 @@ var editmodal = document.querySelector("#edit-modal");
 var closeEditButton = document.querySelector("#close-edit-button");
 var submitedit = document.querySelector("#submit-edit-button");
 
+
+
 closeButton.addEventListener("click", function() {
   modal.classList.toggle("closed");
   modalOverlay.classList.toggle("closed");
@@ -148,6 +211,8 @@ openButton.addEventListener("click", function() {
   modal.classList.toggle("closed");
   modalOverlay.classList.toggle("closed");
 });
+
+
 
 openEditButton.addEventListener("click", function() {
   editmodal.classList.toggle("closed");
@@ -165,8 +230,7 @@ openEditButton.addEventListener("click", function() {
               <option id="${doc.id}">${doc.data().name}</option>
 
         `)
-          
-
+        
           console.log(doc.id, " => ", doc.data());
       });
 
@@ -183,6 +247,102 @@ closeEditButton.addEventListener("click", function() {
 
 submitedit.addEventListener("click", function() {
   editmodal.classList.toggle("closed");
+  modalOverlay.classList.toggle("closed");
+
+  $("#name-select").val("");
+});
+
+
+
+var openaddButton = document.querySelector("#button-add");
+var addmodal = document.querySelector("#modal-add");
+var closeAddButton = document.querySelector("#close-add-button");
+var submitadd = document.querySelector("#submit-add-button");
+
+openaddButton.addEventListener("click", function() {
+  addmodal.classList.toggle("add-closed");
+  modalOverlay.classList.toggle("closed");
+
+  _db
+    .collection("employees")
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        console.log("working");
+
+          $('#name-select').append(`
+
+              <option id="${doc.id}">${doc.data().name}</option>
+
+        `)
+        
+          console.log(doc.id, " => ", doc.data());
+      });
+
+    })
+    
+});
+
+closeAddButton.addEventListener("click", function() {
+  addmodal.classList.toggle("add-closed");
+  modalOverlay.classList.toggle("closed");
+
+  $("#name-select").val("");
+});
+
+submitadd.addEventListener("click", function() {
+  addmodal.classList.toggle("add-closed");
+  modalOverlay.classList.toggle("closed");
+
+  $("#name-select").val("");
+});
+
+
+var opendeleteButton = document.querySelector("#button-delete");
+var deletemodal = document.querySelector("#modal-delete");
+var closedeleteButton = document.querySelector("#close-delete-button");
+var submitdelete = document.querySelector("#submit-delete-button");
+
+opendeleteButton.addEventListener("click", function() {
+  deletemodal.classList.toggle("delete-closed");
+  modalOverlay.classList.toggle("closed");
+
+  _db
+    .collection("employees")
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        console.log("working");
+
+          $('#employee-delete-select').append(`
+
+              <option id="${doc.id}">${doc.data().name}</option>
+
+        `)
+        
+          console.log(doc.id, " => ", doc.data());
+      });
+
+    })
+
+  _db.collection("employees").doc("DC").delete().then(() => {
+    console.log("Document successfully deleted!");
+}).catch((error) => {
+    console.error("Error removing document: ", error);
+});
+
+    
+});
+
+closedeleteButton.addEventListener("click", function() {
+  deletemodal.classList.toggle("delete-closed");
+  modalOverlay.classList.toggle("closed");
+
+  $("#name-select").val("");
+});
+
+submitdelete.addEventListener("click", function() {
+  deletemodal.classList.toggle("delete-closed");
   modalOverlay.classList.toggle("closed");
 
   $("#name-select").val("");
